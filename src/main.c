c75c6e15ec66a96433fe2d18bf2449bacf5cbe0c
Clubdesamis <hidreigon444@hotmail.ca>
2024-12-28 12:29:19 -0500

    cossins

--- src/main.c
+++ src/main.c
@@ -4,10 +4,32 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 
+#define WIDTH 800
+#define HEIGHT 600
+
 int main(int argc, char** argv){
 
     SDL_Window* window;
-    window = SDL_CreateWindow("Clubwindow", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    SDL_Renderer* renderer;
+    SDL_Texture* image;
+    SDL_Rect rect;
+    int width = 0, height = 0;
+
+
+    window = SDL_CreateWindow("Clubwindow", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    image = IMG_LoadTexture(renderer, "/textures/fumo.png");
+    SDL_QueryTexture(image, NULL, NULL, &width, &height);
+
+    rect.x = WIDTH / 2;
+    rect.y = HEIGHT / 2;
+    rect.w = width;
+    rect.h = height;
+
+    while(1){
+
+    }
+
 
     printf("Hello world");
     getchar();
